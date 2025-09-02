48f048105e721055e924d4dbc7aa4c95ddd5c2cc
Otso Saarinen <saarinenotso@gmail.com>
2025-09-02 16:55:53 +0300

    add input type check

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PalkkaLaskin
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+			Console.Write("Enter your hourly wage: ");
+
+            string? userInput = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("No user input");
+                return;
+            }
+
+            float hourlyWage = float.Parse(userInput);
+
+            // calculate the wage per second and print it
+            float wagePerSecond = hourlyWage / 3600;
+            float totalWage = 0;
+
+			while (true)
+            {
+                if (Console.KeyAvailable && Console.ReadKey(true).Key == ConsoleKey.Q)
+                {
+                    break;
+                }
+
+                Console.WriteLine($"Total wage: {Math.Round(totalWage, 2)}€");
+                totalWage = totalWage + wagePerSecond;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
